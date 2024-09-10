a9c41e6f0f4ca25206760703357df4f77ad01cd8
qibas <baskararifqi@gmail.com>
2024-09-10 13:43:09 +0700

    new main

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name_produdct': 'American Express',
+        'name': 'American Express',
         'price': 'Rp28.000',
         'description': 'A strong black coffee drink made from espresso and hot water. Americano has a strong taste, but is not too bitter because it is mixed with water.'
     }

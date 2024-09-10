7f88d89f6ee418b9e9c32c0b56cf2515e7b015bf
qibas <baskararifqi@gmail.com>
2024-09-10 13:39:49 +0700

    new main

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name_app': 'Go Cafe',
-        'name': 'Rajendra Rifqi Baskara',
-        'class': 'PBP C'
+        'name_produdct': 'American Express',
+        'price': 'Rp28.000',
+        'description': 'A strong black coffee drink made from espresso and hot water. Americano has a strong taste, but is not too bitter because it is mixed with water.'
     }
 
     return render(request, "main.html", context)

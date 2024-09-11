f060abec002d766872767d85d34b65eaf461bffc
qibas <baskararifqi@gmail.com>
2024-09-11 11:41:12 +0700

    new main

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class MoodEntry(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()

4f51f669e81a6f7f1717d62c73a27fc7fd94987e
qibas <baskararifqi@gmail.com>
2024-09-22 00:57:28 +0700

    Done Tampilan

--- go_cafe/settings.py
+++ go_cafe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e79a0h&d5u5+arh!sxg#mkm6&-bubq-i0#i5b6r1-iyr61j!$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","rajendra-rifqi-gocafe.pbp.cs.ui.ac.id"]
 

3173e968df7db657ef25a1af3e54960367606850
qibas <baskararifqi@gmail.com>
2024-09-30 23:35:05 +0700

    Temp Tugas 5

--- main/views.py
+++ main/views.py
@@ -11,12 +11,19 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
     product_entry = Product.objects.filter(user=request.user)
+    max_price = 50000
+
+    for product in product_entry:
+        product.price_percentage = min((product.price / max_price) * 100, 100)
+
     context = {
         'name': request.user.username,
         'name_person' : 'Rajendra Rifqi Baskara',
@@ -90,3 +97,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+
+    # Set product entry sebagai instance dari form
+    form = ProductEntryForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+    # Hapus mood
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
